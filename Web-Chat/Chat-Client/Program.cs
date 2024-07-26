39efb8787975231e17061839cb530c553de327e8
김정태.work <jungtae.work@gmail.com>
2024-07-27 01:52:55 +0900

    🎉 웹 채팅 프로젝트 추가

---
+++ Web-Chat/Chat-Client/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Chat_Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Chat("http://localhost:3000");
+
+            Console.WriteLine("Press any key to continue...");
+            Console.Read();
+        }
+
+        async static void Chat(string url)
+        {
+            var client = new SocketIOClient.SocketIO(url);
+
+            client.On("hi", response =>
+            {
+                Console.WriteLine(response);
+
+                string text = response.GetValue<string>();
+                Console.WriteLine(text);
+            });
+
+            client.OnConnected += async (sender, e) =>
+            {
+                await client.EmitAsync("chat message", "socket.io");
+            };
+
+            await client.ConnectAsync();
+        }
+    }
+}
